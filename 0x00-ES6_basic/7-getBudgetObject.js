5768f86f934c367e4dd782b046a320dc6bd06644
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-06 00:32:11 +0100

    get budget object

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+/*eslint-disable*/
+
+export default function getBudgetObject() {
+    const budget = {
+        income: 'income',
+        gdp: 'gdp',
+        capita: 'capita',
+    };
+  
+    return budget;
+}
