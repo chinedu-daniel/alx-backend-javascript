bbfe310dc7a97215f8cf69b4b976bbe343fc133c
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-05 21:31:10 +0100

    ES6 task 0 -  to instantiate const and let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,13 @@
+function taskFirst() {
+	const task = "I prefer const when I can";
+	return task;
+}
+
+module.export = taskFirst;
+
+function taskNext() {
+	let task = "But sometimes let is okay";
+	return task;
+}
+
+module.export = taskName;
