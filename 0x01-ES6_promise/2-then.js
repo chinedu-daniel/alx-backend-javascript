f551582149d0403cdaf541d8e625c631b92276f9
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-07 14:21:54 +0100

    2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+export default function handleResponseFromAPI(promise) {
+	return promise 
+		.then((resolve) => {
+			return {
+				status: 200,
+				body: 'success',
+			};
+		})
+		.catch((error) => {
+			return new Error();
+		})
+		.finally(() => {
+			console.log('Got a response from the API');
+		});
+}
