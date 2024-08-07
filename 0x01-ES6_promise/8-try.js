c79eb644430ce14c19a0441d91f3ad5b73759d18
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-07 20:38:01 +0100

    8-try.js

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+
+export default function divideFunction(numerator, denominator) {
+	if (denominator === 0) throw new Error('cannot divide by 0');
+	return (numerator /denominator);
+}
