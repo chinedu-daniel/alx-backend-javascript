1856fbb0e5365582f29f79278a1c5f0eb9cd4c8e
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-08 13:21:43 +0100

    0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 
 export default class ClassRoom {
-	constructor(maxStudentsSize, _maxStudentsSize) {
-		this.maxStudentsSize = maxStudentsSize;
-		this._maxStudentsSize = _maxStudentsSize;
+	constructor(maxStudentsSize) {
+		this.maxStudentsSize = _maxStudentsSize;
 	}
 };
