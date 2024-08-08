1e867fcd5ddab37f9d8ff3af063e3fec4fe4a438
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-08 16:45:46 +0100

    100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+import Car from './10-car.js'
+
+export default class EVCar extends Car {
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this.range = range;
+	}
+
+	get range() {
+		return this._range;
+	}
+
+	set range(value) {
+		this._range;
+	}
+
+	cloneCar() {
+		const Species = super.constructor[Symbol.species];
+
+		return new Species();
+	}
+}
