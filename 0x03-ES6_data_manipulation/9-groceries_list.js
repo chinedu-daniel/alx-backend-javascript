274a73a5b2f37172005026af53f08742d6c28f29
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-13 14:55:22 +0100

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 
 export default function groceriesList() {
-	const myGroceries = {
-		Apples: 10,
-		Tomatoes: 10,
-		Pasta: 1,
-		Rice: 1,
-		Banana: 5,
-	};
+	const myGroceries = [
+		[Apples, 10],
+		[Tomatoes, 10],
+		[Pasta, 1],
+		[Rice, 1],
+		[Banana, 5],
+	];
 
-	return myGroceries;
+	return new Map(myGroceries);
 }
