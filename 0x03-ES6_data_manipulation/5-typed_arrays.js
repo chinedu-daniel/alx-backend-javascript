f7dafb0c7fce0d07ad48ab9e673a77a12d5a735d
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-14 03:29:05 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,16 @@
 /* eslint-disable */
 
-export default function createInt8TypedArray(length, position, value) {
-	const buffer = new ArrayBuffer(length);
-
-	const int8Array = new int8Array(buffer);
-
-	if (position < 0 || position >= int8Array.length) {
+export default function createInt8TypedArray(length, position, value,) {
+	if (position >= length) {
 		throw new Error('Position outside range');
 	}
+ 
+	const buffer = new DataView(
+		new ArrayBuffer(length),
+		0,
+		length,
+	);
 
-	int8Array[position] = value;
-
+	buffer.setUint8(position, value);
 	return buffer;
 }
