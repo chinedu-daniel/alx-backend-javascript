dda069fa868d84290fd73e7846d1f51478244957
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-14 03:40:34 +0100

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,7 @@ export const weakMap = new WeakMap();
 
 const MAX_ENDPOINT_CALLS = 5;
 
-export default function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
 	if (!weakMap.has(endpoint)) {
 		weakMap.set(endpoint, 0);
 	}
