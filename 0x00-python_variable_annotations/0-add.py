3c5caa355b0293171baf09dc52113f5eb086e2a6
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-15 13:33:13 +0100

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''a type-annotated function that takes a float and returns their sum'''
+
+def add(float: a, b) -> float:
+    '''the class that takes this input'''
+    return a + b;
