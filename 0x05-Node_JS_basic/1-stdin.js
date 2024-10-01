fc1304a4049b6dc549c78f71d19b386105bdca19
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-10-01 09:31:59 +0100

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,10 +4,10 @@
 process.stdin.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', function (data) {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
+  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.exit();
 });
 
 process.on('exit', function () {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
