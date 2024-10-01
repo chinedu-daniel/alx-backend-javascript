99a5c7081649bb1749d27b3f87ee4329c2cba655
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-10-01 22:01:25 +0100

    task 1 corrected

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,11 +7,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Listen for input from the user
-process.stdin.on('data', (data) => {
-  const inputName = data.toString().trim(); // Convert input to string and trim any extra spaces
-  process.stdout.write(`Your name is: ${inputName}\n`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-  // Close the input stream and print the closing message
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
