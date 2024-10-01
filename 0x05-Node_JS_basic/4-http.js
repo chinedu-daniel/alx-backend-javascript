2e242706c2f920d82886badd2b5b86e4c70bc0a0
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-10-01 11:38:07 +0100

    Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Create a small HTTP server using Node's HTTP module
+ */
+const http = require('http');
+const app = http.createServer();
+
+app.on('request', function (req, res) {
+  res.write('Hello Holberton School!');
+  res.end()
+});
+
+app.listen(1245);
+
+module.exports = app
