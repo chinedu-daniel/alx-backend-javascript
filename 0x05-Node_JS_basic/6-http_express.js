6d69c2a1a00f80fb63e0646507c8f0fb0ff9e7a7
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-10-01 22:57:47 +0100

    fixed task 5

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 1245;
 
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
   res.send('Hello Holberton School!');
 });
 
