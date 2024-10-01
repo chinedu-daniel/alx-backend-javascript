1131eb99153fc9fcfad8076db944cf7caaf21791
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-10-01 13:45:59 +0100

    Create a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const app = express();
+const PORT = 1245;
 
 app.get('/', function (req, res) {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
