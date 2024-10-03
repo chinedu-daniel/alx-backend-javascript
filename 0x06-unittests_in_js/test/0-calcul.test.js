424c6f14f2f2f225f4b02ad82ba556ae05cda587
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-10-03 07:48:18 +0100

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/test/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('../0-calcul');
+
+describe('calculateNumber', function() {
+    it('should return 4 when inputs are 1.4 and 3.3', function() {
+        assert.strictEqual(calculateNumber(1.4, 3.3), 4);
+    });
+
+    it('should return 9 when inputs are 4.6 and 4.4', function() {
+        assert.strictEqual(calculateNumber(4.6, 4.4), 9);
+    });
+
+    it('should return 6 when inputs are 2.5 and 2.5', function() {
+        assert.strictEqual(calculateNumber(2.5, 2.5), 6);
+    });
+
+    it('should return 0 when inputs are 1.4 and -1.4', function() {
+        assert.strictEqual(calculateNumber(1.4, -1.4), 0);
+    });
+})
